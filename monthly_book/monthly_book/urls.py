4ca637be5b22752e6c497bc61d5112169e40e6db
Sumit Shrivastava <c0d3sh3lf@gmail.com>
2021-01-05 22:32:45 +0400

    Created App User Management

--- monthly_book/monthly_book/urls.py
+++ monthly_book/monthly_book/urls.py
@@ -20,5 +20,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('', include(('mbook.urls', 'mbook'), namespace='mbook')),
+    path('umanage/', include(('user_management.urls', 'user_management'), namespace='user_management')),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
