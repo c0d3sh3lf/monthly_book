233f7b1ce190b5bcf1900504cfbb764f678cf6e3
Sumit Shrivastava <c0d3sh3lf@gmail.com>
2021-01-09 00:08:41 +0400

    Created edit user and sign_up forms

--- monthly_book/user_management/forms.py
+++ monthly_book/user_management/forms.py
@@ -33,3 +33,79 @@ class UserPasswordChangeForm(PasswordChangeForm):
             user.save()
 
         return user
+
+
+class UserUpdateForm(UserChangeForm):
+    email = forms.EmailField(required=True, widget=forms.EmailInput(attrs={
+        'class':'form-control',
+        'placeholder':'someone@domain.com'
+    }), label="Email")
+
+    first_name = forms.CharField(required=True, widget=forms.TextInput(attrs={
+        'class':'form-control',
+        'placeholder':'John'
+    }), label="First Name")
+
+    last_name = forms.CharField(required=True, widget=forms.TextInput(attrs={
+        'class':'form-control',
+        'placeholder':'Doe'
+    }), label="Last Name")
+
+    is_active = forms.BooleanField(required=False, widget=forms.CheckboxInput(), label='Active?')
+
+    is_superuser = forms.BooleanField(required=False, widget=forms.CheckboxInput(), label='Superuser?')
+
+    is_staff = forms.BooleanField(required=False, widget=forms.CheckboxInput(), label='Staff?')
+
+    class Meta:
+        model = User
+        fields = ('first_name', 'last_name', 'email', 'is_active', 'is_superuser', 'is_staff')
+
+    def __init__(self, *args, **kwargs):
+        super(UserUpdateForm, self).__init__(*args, **kwargs)
+        self.fields.pop('password')
+
+
+class UserSignUpForm(UserCreationForm):
+    email = forms.EmailField(required=True, widget=forms.EmailInput(attrs={
+        'class':'form-control',
+        'placeholder':'someone@domain.com'
+    }))
+    first_name = forms.CharField(required=True, widget=forms.TextInput(attrs={
+        'class':'form-control',
+        'placeholder':'John'
+    }))
+    last_name = forms.CharField(required=True, widget=forms.TextInput(attrs={
+        'class':'form-control',
+        'placeholder':'Doe'
+    }))
+
+    password1 = forms.CharField(required=True, widget=forms.PasswordInput(attrs={
+        'class':'form-control',
+        'placeholder':'Password'
+    }), help_text="<ul><li>Your password can’t be too similar to your other personal information.</li><li>Your password must contain at least 8 characters.</li><li>Your password can’t be a commonly used password.</li><li>Your password can’t be entirely numeric.</li></ul>",
+    label="Password")
+
+    password2 = forms.CharField(required=True, widget=forms.PasswordInput(attrs={
+        'class':'form-control',
+        'placeholder':'Password'
+    }), help_text="This should match the password in the Password field.",
+    label="Confirm Password")
+
+    class Meta:
+        model = User
+        fields = ('first_name', 'last_name', 'email', 'password1', 'password2')
+
+    def save(self, commit=True):
+        user = super(UserSignUpForm, self).save(commit=False)
+        user.email = self.cleaned_data['email']
+        user.username = user.email
+        user.first_name = self.cleaned_data['first_name']
+        user.last_name = self.cleaned_data['last_name']
+        user.is_superuser = False
+        user.is_staff = False
+
+        if commit:
+            user.save()
+
+        return user
--- monthly_book/user_management/urls.py
+++ monthly_book/user_management/urls.py
@@ -5,7 +5,10 @@ urlpatterns = [
     path('login/', views.user_login, name="login"),
     path('logout/', views.user_logout, name="logout"),
     path('change_password/', views.change_password, name="change_password"),
+    path('tnc/', views.tnc, name="tnc"),
+    path('sign_up/', views.sign_up, name="sign_up"),
     path('list_users/', views.list_users, name="list_users"),
     path('view_user/<int:id>/', views.view_user, name="view_user"),
+    path('update_user/<int:id>', views.update_user, name="update_user"),
     path('delete_user/<int:id>/', views.delete_user, name="delete_user"),
 ]
